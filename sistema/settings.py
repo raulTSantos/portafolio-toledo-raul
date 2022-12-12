93a292be4e637a09cc6bc43ecdc88133c666a192
raulTSantos <confox24.s@gmail.com>
2022-12-11 21:57:54 -0500

    static files dir

--- sistema/settings.py
+++ sistema/settings.py
@@ -141,5 +141,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'')
 MEDIA_URL= '/images/'
 
 #archivos staticos
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "images")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
