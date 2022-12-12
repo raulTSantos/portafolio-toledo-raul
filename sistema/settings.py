534f634c5ebeb0986825f4b12180ba5b879000c9
raulTSantos <confox24.s@gmail.com>
2022-12-11 21:15:06 -0500

    dbug true

--- sistema/settings.py
+++ sistema/settings.py
@@ -28,7 +28,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-q2m9gj&mvlr*eekpiefvgh**diq)diy6g+p(1v#5&2*6d(08us'
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
